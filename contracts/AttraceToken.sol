70796f34c92309bc79ebcc613861b795b93cd94a
bobbyray1005 <braythriz4@gmail.com>
2018-03-05 01:06:43 +0100

    Updated contract with transfer lock and whitelisting

--- contracts/AttraceToken.sol
+++ contracts/AttraceToken.sol
@@ -1,10 +1,9 @@
 pragma solidity ^0.4.18;
 
-import '../node_modules/zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
-import '../node_modules/zeppelin-solidity/contracts/lifecycle/Pausable.sol';
+import '../node_modules/zeppelin-solidity/contracts/token/ERC20/PausableToken.sol';
 
 // AttraceToken
-contract AttraceToken is StandardToken, Pausable {
+contract AttraceToken is PausableToken {
     string public constant name = "Attrace";
     string public constant symbol = "ATTR";
     uint32 public constant decimals = 18;
@@ -12,9 +11,9 @@ contract AttraceToken is StandardToken, Pausable {
     uint256 private initialSupply = 1000000000E18;
 
     // Token trading can start after ICO has ended
-    bool    private released = false;             
+    bool public transfersEnabled = false;
     
-    // Allowed addresses to transfer tokens regardless of the lock down period. Presale, crowdsale and Attrace project for initial token distribution.
+    // Attrace can allow partical addresses (our crowdsale contract) to transfer tokens despite the lock up period.
     mapping (address => bool) public transferWhitelist;
 
     function AttraceToken() public {
@@ -22,16 +21,16 @@ contract AttraceToken is StandardToken, Pausable {
       totalSupply_ = initialSupply;
     }
     
-    function transfer(address _to, uint256 _value) canTransfer(msg.sender) whenNotPaused public returns (bool) {
+    function transfer(address _to, uint256 _value) canTransfer(msg.sender) public returns (bool) {
         return super.transfer(_to, _value);
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) canTransfer(_from) whenNotPaused public returns (bool) {
+    function transferFrom(address _from, address _to, uint256 _value) canTransfer(_from) public returns (bool) {
         return super.transferFrom(_from, _to, _value);
     }
 
     modifier canTransfer(address _sender) {
-      if (!released) {
+      if (!transfersEnabled) {
           if (!transferWhitelist[_sender]) {
               revert();
           }
@@ -39,34 +38,24 @@ contract AttraceToken is StandardToken, Pausable {
       _;
     }
 
-    // Owner can allow a particular address (a crowdsale contract) to transfer tokens despite the lock up period
-    function setTransferWhitelist(address addr, bool state) onlyOwner whenNotReleased public {
-      transferWhitelist[addr] = state;
-    }
-
-    modifier whenNotReleased() {
-      if (released == true) {
+    modifier whenTransfersDisabled() {
+      if (transfersEnabled) {
           revert();
       }
       _;
     }
+
+    function setTransferWhitelistAddress(address addr, bool state) onlyOwner whenTransfersDisabled public {
+      transferWhitelist[addr] = state;
+    }
   
-    // Owner can release the tokens after trading once
-    function releaseTokenTransfer() public onlyOwner {
-      released = true;
+    // Attrace can enable token trading once
+    function setTransfersEnabled() public onlyOwner {
+      transfersEnabled = true;
     }
 
-    // // Attrace project will enable the token for trading after the ICO has ended
-    // modifier isIncubated() {
-    //   require(incubated == true);
-    //   require(block.timestamp > incubationTime);
-    //   _;
-    // }
-
-    // function incubate(uint256 _incubationTime) onlyOwner public {
-    //   require(incubated == false);
-    //   require(_incubationTime > block.timestamp);
-    //   incubated = true;
-    //   incubationTime = _incubationTime;
-    // }
+    /** Accessor functions for testing -- can be removed at deploy time **/
+    function getAddressTransferWhitelistStatus(address addr) public view returns (bool) {
+      return transferWhitelist[addr];
+    }
 }
